086de3f55cee2e6a29cdea046216d0b852380be6
Nicolina Diana <diana.nicolina@libero.it>
2014-10-09 08:19:41 +0000

    aggiornamento

---
+++ src/main/java/com/github/nicolediana/Main.java
@@ -0,0 +1,15 @@
+package com.github.nicolediana;
+
+import com.sun.org.apache.xml.internal.serializer.utils.StringToIntTable;
+
+import java.awt.*;
+
+/**
+ * Created by pcnicole on 09/10/2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SystemColor.out.println(" HELLO Morfeo!!!!");
+    }
+}
